c95f1226132fa25cfd123ca00838a9fac07a3b50
rcintron1 <rolando.cintron@gmail.com>
2017-09-12 23:35:30 -0400

    added target on new page (line68) and set default count value to 5

--- scripts.js
+++ scripts.js
@@ -15,30 +15,30 @@ $(document).ready(function() {
 });
 
 function addBtnEvent() {
-	// below I'm adding the Search button click
+  // below I'm adding the Search button click
   $(".btn-primary").on("click", function() {
-		// transfering values from form to local variables
+    // transfering values from form to local variables
     var keyWord = $(".input-subject").val();
     var beginDate = $(".input-timeframe").val();
     var endDate = $(".input-timeframe").val();
     var rowCount = $(".dropdown-menu").val();
-		// sending local variables to the newsFeed.getData ajax function
+    // sending local variables to the newsFeed.getData ajax function
     newsFeed.getData(keyWord, beginDate, endDate, rowCount);
   });
-	$(".dropdown-menu li").on('click', function() {
+  $(".dropdown-menu li").on('click', function() {
     newsFeed.count = parseInt($(this).text());
   });
 }
 
 newsFeed = {
   rawData: '', //this is where I catch the ajax raw data
-  count: '', //this is where I get the # of records->**id's preferred
+  count: 5, //this is where I get the # of records->**id's preferred
   getData: function(keyWord, beginDate, endDate, count) {
     var queryOptions = {};
     queryOptions['api-key'] = "b2dde2ff614e4953a503afd7c8887f88";
     // newsFeed.count = count; if I can get li by id's this is what I would do
-		// ternary operators to get query options for ajac call
-		keyWord ? queryOptions['q'] = keyWord : '';
+    // ternary operators to get query options for ajac call
+    keyWord ? queryOptions['q'] = keyWord : '';
     beginDate ? queryOptions['begin_date'] = beginDate : '';
     endDate ? queryOptions['end_date'] = endDate : '';
 
@@ -62,7 +62,11 @@ newsFeed = {
     table.append("<thead><th>Links</th></thead>");
     for (var i = 0; i < newsFeed.count; i++) {
       var row = $('<tr>');
-      row.append(newsArr[i].headline["main"]);
+      var link = $('<a>');
+      link.prop("href", newsArr[i].web_url);
+      link.text(newsArr[i].headline["main"]);
+			link.attr('target', '_blank');
+      row.append(link.append(link));
       table.append(row);
     }
     $("#content02").html(table);
